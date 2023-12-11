ee5b6031b6b4bdf6e72666b8246302fde7554dea
ERMIASWAKGARI <ermiaswakgarisse5@gmail.com>
2023-12-11 10:00:06 -0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ef82fc98-2c34-4b44-9f98-59b23e4103ea';
+export const MODAL_CLOSE_SEC = 2.5;
